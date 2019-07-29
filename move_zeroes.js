42e365d912834e147b07eddff45a3eae937b29ca
clairemuller <clairermuller@gmail.com>
2019-07-28 22:03:29 -0700

    solved move zeroes, needs work

--- move_zeroes.js
+++ move_zeroes.js
@@ -7,8 +7,33 @@
 // Input: [0,1,0,3,12]
 // Output: [1,3,12,0,0]
 
+// first attempt
+// save the original nums length, iterate over nums array
+// if element is 0, splice it out
+// then add zeroes until nums is original length
 
-
+// splicing the zero and pushing it onto the array doesn't work
+// concat doesn't work
+// nums.push(nums.splice(i, 1))
+// returns [1,3,12,[0],[0]]
 var moveZeroes = function(nums) {
+  let i = 0;
+  let l = nums.length;
 
+  while (i < nums.length) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+
+  while (nums.length < l) {
+    nums.push(0);
+  }
+
+  return nums;
 };
+
+let nums = [0,1,0,3,12];
+moveZeroes(nums);
