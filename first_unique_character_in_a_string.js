6d0bf9d40b4e705fcc8026663b8b782a483fd01b
clairemuller <clairermuller@gmail.com>
2019-07-28 19:06:38 -0700

    solved first unique

--- first_unique_character_in_a_string.js
+++ first_unique_character_in_a_string.js
@@ -1,7 +1,7 @@
-// NOT SOLVED
+// SOLVED
 
 // Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
-// You may assume the string contain only lowercase letters.
+// You may assume the string contains only lowercase letters.
 
 // s = "leetcode"
 // return 0.
@@ -9,6 +9,25 @@
 // s = "loveleetcode",
 // return 2.
 
+// first attempt
+// turn string into array, iterate through and count each letter
+// iterate through again until we find the first letter with a count of 1
 var firstUniqChar = function(s) {
+  let count = {};
+  let arr = s.split('');
 
+  for (letter of arr) {
+    count[letter] = (count[letter] || 0) + 1;
+  }
+
+  for (letter of arr) {
+    if (count[letter] === 1) {
+      return arr.indexOf(letter)
+    }
+  }
+
+  return -1;
 };
+
+let s = "loveleetcode";
+firstUniqChar(s);
