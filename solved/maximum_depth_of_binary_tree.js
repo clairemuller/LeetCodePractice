ca098a2857cb0e029531b131e5d941a77081f48d
clairemuller <clairermuller@gmail.com>
2019-07-29 23:55:46 -0700

    recursion

--- solved/maximum_depth_of_binary_tree.js
+++ solved/maximum_depth_of_binary_tree.js
@@ -25,18 +25,15 @@ let node15 = new TreeNode(15);
 let node7 = new TreeNode(7);
 node3.left = node9;
 node3.right = node20;
-node9.left = node15;
 node20.left = node15;
 node20.right = node7;
 
+// recursion
 var maxDepth = function(root) {
-  if (!root) {
-    return 0;
-  }
+  if (!root) return 0;
   let depthLeft = maxDepth(root.left);
   let depthRight = maxDepth(root.right);
-
-  return Math.max(depthLeft, depthRight)+1;
+  return Math.max(depthLeft, depthRight) + 1;
 };
 
-maxDepth(node3)
+maxDepth(node3);
