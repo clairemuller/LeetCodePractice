fc02bc94865a5e565c99714853bd11f4408a02a0
clairemuller <clairermuller@gmail.com>
2019-08-03 12:28:47 -0700

    solved longest common prefix

--- longest_common_prefix.js
+++ longest_common_prefix.js
@@ -7,6 +7,27 @@
 // Input: ["flower","flow","flight"]
 // Output: "fl"
 
+// first attempt
+// set prefix equal to the first string
+// start for loop at next string
+// start comparing each letter
 var longestCommonPrefix = function(strs) {
+  if (!strs || !strs.length) {
+    return "";
+  }
+  let prefix = strs[0];
 
+  for (var i = 1; i < strs.length; i++) {
+    let currStr = strs[i];
+    for (let j = 0; j < prefix.length; j++) {
+      if (prefix[j] != currStr[j]) {
+        prefix = currStr.slice(0, j);
+      }
+    }
+  }
+  return prefix;
 };
+
+// let strs = ["flower","flow","flight"];
+let strs = ["ca","a"]
+longestCommonPrefix(strs);
