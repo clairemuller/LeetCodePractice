ac16cb4f9aaebbf23a0af76e2692352047ee0c90
clairemuller <clairermuller@gmail.com>
2019-08-07 14:37:05 -0700

    working on count and say, super hard

--- count_and_say.js
+++ count_and_say.js
@@ -19,6 +19,69 @@
 // Input: 4
 // Output: "1211"
 
+// first attempt
+// create object
+// start count from 1 with initial string
+// while count is less than given integer n
+// split current string into an array
+// overwrite string
+// push new numbers into string
+// turn number string back into word string
 var countAndSay = function(n) {
-
+  let count = 1;
+  let str = 'one 1';
+  while (count < n) {
+    str = wordsToNumbers(str);
+    count++;
+    str = numbersToWords(str);
+    count++;
+  }
+  return str;
 };
+
+function wordsToNumbers(str) {
+  let obj = { 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5' }
+  let arr = str.split(' ');
+  str = '';
+  // turn word string into number string
+  for (word of arr) {
+    if (obj[word]) {
+      str += obj[word];
+    } else {
+      // if number has s on the end, chop it off
+      if (word[word.length-1] === 's') {
+        word = word.substring(0, word.length-1)
+      }
+      str += word;
+    }
+  }
+  return str;
+}
+
+function numbersToWords(str) {
+  let obj = { '1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five' }
+  let wordStr = '';
+  let i = 0;
+  let count = 0;
+
+  while (i < str.length) {
+    if (str[i] === str[i+1]) {
+      count++;
+    } else {
+      count++;
+      word = count.toString();
+      wordStr += obj[word] + ' ' + str[i];
+      if (count !=1) {
+        wordStr += 's '
+      } else {
+        wordStr += ' '
+      }
+      count = 0;
+    }
+    i++;
+  }
+
+  return wordStr.trim();
+}
+
+countAndSay(6)
