14ee294b464b1463035893a2b78872cbc10b578b
clairemuller <clairermuller@gmail.com>
2019-08-15 20:39:02 -0700

    solved reverse integer

--- reverse_integer.js
+++ reverse_integer.js
@@ -1,20 +1,18 @@
-// NOT SOLVED
+// SOLVED
 
 // Given a 32-bit signed integer, reverse digits of an integer.
 // Input: 123
 // Output: 321
 
 var reverse = function(x) {
-  x = parseInt(x.toString().split('').reverse().join(''));
-  if (Math.abs(x) > 2e31) {
+  let sign = x < 0;
+  x = parseInt(x.toString().split('').reverse().join(''), 10);
+  if (x > Math.pow(2, 31)) {
     return 0
   } else {
-    return x
+    if (sign) {
+      return -x
+    }
+    return x;
   }
 };
-
-let x = 128938498340988773884849328490840955484989780;
-reverse(num);
-
-
-x = x.toString().split('').reverse().join('');
