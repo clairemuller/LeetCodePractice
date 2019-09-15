dec1f2904c1da6c4e280df18628fbd77db0a59e0
clairemuller <clairermuller@gmail.com>
2019-09-14 19:08:32 -0700

    add string to integer

---
+++ string_to_integer.js
@@ -0,0 +1,24 @@
+// NOT SOLVED
+
+// Implement atoi which converts a string to an integer.
+// The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+// If no valid conversion could be performed, a zero value is returned.
+
+// Input: "   -42"
+// Output: -42
+
+// Input: "4193 with words"
+// Output: 4193
+
+
+// Input: "words and 987"
+// Output: 0
+
+// Input: "-91283472332"
+// Output: -2147483648
+// Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
+// Thefore INT_MIN (−2^31) is returned.
+
+var myAtoi = function(str) {
+
+};
